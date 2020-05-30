8ac8281b30c0d8d537e2c74b10471005d7521473
Avinash Gupta <43546989+avigupta24@users.noreply.github.com>
2020-05-30 19:56:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+
+#app=Flask(__name__,template_folder='template') if default folder is not used then ....
+
+app=Flask(__name__)
+
+var1=[
+    {
+    'name':'Avinash',
+    'job' : 'DBA'
+    },
+    {
+        'name':'Priya',
+        'job':'Housewife'
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template('home.html',posts=var1)
+
+@app.route("/about/")
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
