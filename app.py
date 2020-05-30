e42203617065cd6a9986fb76490f3c5868c99647
Avinash Gupta <guptaavilnct@gmail.com>
2020-05-30 20:02:57 +0530

    just for testing from desktop to git

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template
 
 #app=Flask(__name__,template_folder='template') if default folder is not used then.... updated in git
 
+####one change done in desktop and push to git
+
 app=Flask(__name__)
 
 var1=[
